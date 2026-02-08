773278087ea4f512770cd048bbc8b1a3c61d1b11
Oleha(Вадим) <72513443+Oleha1@users.noreply.github.com>
2026-02-09 01:40:03 +0500

    Update MajesticRPSANGRight.plugin.js

--- MajesticRPSANGRight.plugin.js
+++ MajesticRPSANGRight.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name MajesticRPSANGRight
  * @author Oleha discrod: mroleha
- * @version 1.0.3
+ * @version 1.0.4
  * @description Majestic RP Washington SANG Right Click version.
  * @source https://github.com/Oleha1/BDPlugin
  * @updateUrl https://raw.githubusercontent.com/Oleha1/BDPlugin/main/MajesticRPSANGRight.plugin.js
@@ -13,7 +13,7 @@ const TARGET_CHANNEL_ID_KA = "1214393282201919542"
 
 const TARGET_CHANNEL_ID_MI = "1214393282201919543";
 
-const PLUGIN_VERSION = "1.0.3";
+const PLUGIN_VERSION = "1.0.4";
 const UPDATE_URL = "https://raw.githubusercontent.com/Oleha1/BDPlugin/main/MajesticRPSANGRight.plugin.js";
 const PLUGIN_FILE_NAME = "MajesticRPSANGRight.plugin.js";
 
@@ -52,6 +52,8 @@ module.exports = (() => {
 
 			const remoteVersion = match[1];
 
+			console.log("test")
+			
 			if (remoteVersion !== PLUGIN_VERSION) {
 				BdApi.UI.showConfirmationModal(
 					"Доступно обновление",
@@ -609,6 +611,7 @@ module.exports = (() => {
 		};
 	})(window.BDFDB_Global.PluginUtils.buildPlugin({}));
 })();
+
 
 
 
