b488b7f8bb6c0f24bf220400402f46fd1dc1b1c3
Oleha(Вадим) <72513443+Oleha1@users.noreply.github.com>
2026-02-09 01:44:38 +0500

    Update MajesticRPSANGRight.plugin.js

--- MajesticRPSANGRight.plugin.js
+++ MajesticRPSANGRight.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name MajesticRPSANGRight
  * @author Oleha discrod: mroleha
- * @version 1.0.4
+ * @version 1.0.3
  * @description Majestic RP Washington SANG Right Click version.
  * @source https://github.com/Oleha1/BDPlugin
  * @updateUrl https://raw.githubusercontent.com/Oleha1/BDPlugin/main/MajesticRPSANGRight.plugin.js
@@ -13,7 +13,7 @@ const TARGET_CHANNEL_ID_KA = "1214393282201919542"
 
 const TARGET_CHANNEL_ID_MI = "1214393282201919543";
 
-const PLUGIN_VERSION = "1.0.4";
+const PLUGIN_VERSION = "1.0.3";
 const UPDATE_URL = "https://raw.githubusercontent.com/Oleha1/BDPlugin/main/MajesticRPSANGRight.plugin.js";
 const PLUGIN_FILE_NAME = "MajesticRPSANGRight.plugin.js";
 
@@ -52,18 +52,16 @@ module.exports = (() => {
 
 			const remoteVersion = match[1];
 
-			console.log("test")
-			
 			if (remoteVersion !== PLUGIN_VERSION) {
 				BdApi.UI.showConfirmationModal(
 					"Доступно обновление",
 					`Текущая версия: ${PLUGIN_VERSION}\nНовая версия: ${remoteVersion}`,
 					{
 						confirmText: "Обновить",
-						cancelText: "Отмена",
+						cancelText: null,
 						onConfirm: () => {
 							const fs = require("fs");
-							const path = require("path");
+							const path = require("path"); 
 
 							const pluginPath = path.join(BdApi.Plugins.folder, PLUGIN_FILE_NAME);
 							fs.writeFileSync(pluginPath, text);
@@ -611,7 +609,3 @@ module.exports = (() => {
 		};
 	})(window.BDFDB_Global.PluginUtils.buildPlugin({}));
 })();
-
-
-
-
